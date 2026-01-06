2a966c0a57e04491d69ffa836da97728eb9a0175
adi23kharat <147171826+adi23kharat@users.noreply.github.com>
2026-01-07 02:34:09 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://real-time-chat-app-fybr.onrender.com",
   credentials:true
 })) 
 app.use(express.urlencoded({extended : true}))
